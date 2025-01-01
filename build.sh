c1c8ff6c15f01f8a8fd389bd5fee9c2dfac68a3e
DXXN <pexwp@mail.ru>
2025-01-02 02:37:49 +0300

    fixed build script

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
-echo "Debug or Release [D] or [R]?"
-select choise in "D" "R"; do
-    case $choise in
-        D ) mkdir build; 
+echo "Debug or Release?"
+select choise in D R
+do
+    case $REPLY in
+        D) mkdir build; 
 	      cmake -S . -B build/ -D CMAKE_BUILD_TYPE=Debug;
 	      cmake --build build/debug;
-	      cmake --install . --prefix "../../bin"
 	      break;;
-        R ) mkdir build; 
+        R) mkdir build; 
 	      cmake -S . -B build/ -D CMAKE_BUILD_TYPE=Release;
 	      cmake --build build/release;
-	      cmake --install . --prefix "../../bin";
-          break;;
+          break;;	
+		*)
+        echo "Unknown symbol $REPLY";
+		break;;
     esac
 done
